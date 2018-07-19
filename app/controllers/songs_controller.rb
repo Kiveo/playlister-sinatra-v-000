d0b76898193f0ff267afc4b9a5d0fdc56bbecf2e
Maverick Tesini <mavericktesini@gmail.com>
2018-07-19 20:48:24 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -41,7 +41,7 @@ class SongsController < ApplicationController
     binding.pry
     @song = Song.find_by_slug(params[:slug])
     @song.update(params[:song])
-    @song.artist = Artist.find_or_create_by(name: params["artist[name]"])
+    @song.artist = Artist.find_or_create_by(name: params["artist"]["name"])
     @song.genre_ids = params[:genres]
     @song.save
 
